496d2c49e897bfb5434b51dc56f577e902784804
Pedro Kalil <pedkag@gmail.com>
2019-01-03 05:25:14 -0200

    melhorias visando a código mais funcional e organizado

--- fhdataapi/IBGE/APISIDRA.py
+++ fhdataapi/IBGE/APISIDRA.py
@@ -8,48 +8,96 @@ from bs4 import BeautifulSoup as soup
 
 import consultaSIDRA
 
+print('> Utilize a função "core()" diretamente, ou a função "guided()" para uma aplicação guiada.')
+print('> Utilize "agr_table()" para consultar os agregados disponíveis.')
+
 #######
 # IMPORTANTE: A atual composição desse arquivo visa uma interface mais "user-friendly".
 # Pensando nisso, tranformei o modelo passado (no qual o usuário deveria chamar uma função com as
 # variáveis desejadas) em um modelo no qual o usuário vai respondendo a pedidos de inputs.
 # Acredito, entretanto, que esse formato não seja o mais adequado.
-
 #######
 
-print('Em caso de dúvidas ao longo da inserção dos parâmetros, consulte http://api.sidra.ibge.gov.br/home/ajuda')
+print('\n*Em caso de dúvidas ao longo da inserção dos parâmetros, consulte http://api.sidra.ibge.gov.br/home/ajuda')
 
 # https://servicodados.ibge.gov.br/api/v3/agregados    ####   dados agregados
 # http://api.sidra.ibge.gov.br/    ####   consulta SIDRA
 # http://api.sidra.ibge.gov.br/home/ajuda    ###   documentação API SIDRA
 
-def conector():
+def agr_table():
+
+    agregados = requests.get('https://servicodados.ibge.gov.br/api/v3/agregados')
+    agregadosdata = json.loads(agregados.text)
+    pesq_agr=[]
+    for i in range(len(agregadosdata)):
+        for j in range(len(agregadosdata[i]["agregados"])):
+            id_pesquisa = (agregadosdata[i]["id"])
+            nome_pesquisa = (agregadosdata[i]["nome"])
+            id_agregado = (agregadosdata[i]["agregados"][j]["id"])
+            nome_agregado = (agregadosdata[i]["agregados"][j]["nome"])
+            pesq_agr.append({'ID pesquisa': id_pesquisa, 'Nome pesquisa': nome_pesquisa, 'ID agregado': id_agregado,
+                            'Nome agregado': nome_agregado})
+    df_pesq_agr = pd.DataFrame(pesq_agr)
+    print(df_pesq_agr)
+
+
+def treat(resp):
+
+    dflist = []
+
+    for d in resp[1:]:
+        s = pd.Series(d)
+        dflist.append(s.to_frame().T)
+
+    df = pd.concat(dflist)
+
+    df.columns = df.columns.map(resp[0])
+
+    # Linhas de código para tratamento de variáveis removidas até que eu compreenda se tal tratamento seria possível.
+
+    #pd.options.display.max_columns = 14
+
+    ### Corrige o índice de entradas do DataFrame
+    df.to_dict('records')
+    df = df.reset_index(drop=True)
+    #print(df.dtypes)
+    print(df.head())
+    print(df.tail())
+
+def guided():
 
     url = 'http://api.sidra.ibge.gov.br/values/'
 
     # Primera parte da função pede ao usuário o nome do agregado de dados desejado, e obtém o ID.
     # Esse ID obtido permite o acesso ao documento com as variáveis disponíveis para aquele agregado.
-    # Ex1: "IPCA - Variação mensal, para o índice geral, grupos, subgrupos, itens e subitens de produtos e serviços (de julho/1989 até dezembro/1990)"
+    # Ex1: "Número de estabelecimentos agropecuários, com agricultura familiar e não familiar, e Área dos estabelecimentos por grupos de atividade econômica, condição produtor em relação às terras e tipo de prática agrícola - (MDA)"
     # Ex2: "Comercialização de agrotóxicos e afins, total e por área plantada, segundo a classe de uso"
+    # Ex3: "IPCA dessazonalizado - Variação mensal, acumulada no ano e peso mensal, para o índice geral, grupos, subgrupos, itens e subitens de produtos e serviços (a partir de janeiro/2012)"
 
     # t -> código do agregado de onde serão retirados os dados para as variáveis desejadas. Pode ser obtido pela API de agregados.
     # (Ex: '1327')
 
-    nome = input('\nInsira o nome da tabela de dados agregagos. >>>')
-    pesqid = None
-    agregados = requests.get('https://servicodados.ibge.gov.br/api/v3/agregados')
-    agregadosdata = json.loads(agregados.text)
-    for i in range(len(agregadosdata)):
-        for j in range(len(agregadosdata[i]["agregados"])):
-            if agregadosdata[i]["agregados"][j]["nome"] == str(nome):
-                pesqid = (agregadosdata[i]["agregados"][j]["id"])
-                break
-            elif agregadosdata[i]["agregados"][j]["nome"] != str(nome) and j == (
-                    len(agregadosdata[i]["agregados"][j]) - 1):
-                continue
-            elif agregadosdata[i]["agregados"][j]["nome"] != str(nome) \
-                    and i == (len(agregadosdata) - 1) \
-                    and j == (len(agregadosdata[i]["agregados"][j]) - 1):
-                print("Não há esse agregado de dados na base disponível")
+    nome = input('\nInsira o nome ou ID da tabela de dados agregados. >>>')
+
+    if nome.isdigit():
+        pesqid = nome
+
+    else:
+        pesqid = None
+        agregados = requests.get('https://servicodados.ibge.gov.br/api/v3/agregados')
+        agregadosdata = json.loads(agregados.text)
+        for i in range(len(agregadosdata)):
+            for j in range(len(agregadosdata[i]["agregados"])):
+                if agregadosdata[i]["agregados"][j]["nome"] == str(nome):
+                    pesqid = (agregadosdata[i]["agregados"][j]["id"])
+                    break
+                elif agregadosdata[i]["agregados"][j]["nome"] != str(nome) and j == (
+                        len(agregadosdata[i]["agregados"][j]) - 1):
+                    continue
+                elif agregadosdata[i]["agregados"][j]["nome"] != str(nome) \
+                        and i == (len(agregadosdata) - 1) \
+                        and j == (len(agregadosdata[i]["agregados"][j]) - 1):
+                    print("Não há esse agregado de dados na base disponível")
 
     consultaSIDRA.code = pesqid
     if pesqid != None:
@@ -132,35 +180,83 @@ def conector():
 
     data = requests.get(url)
     datares = json.loads(data.text)
+    treat(datares)
     return datares
 
-########
-
-def treat():
-
-    resp = conector()
-
-    dflist = []
-
-    for d in resp[1:]:
-        s = pd.Series(d)
-        dflist.append(s.to_frame().T)
-
-    df = pd.concat(dflist)
-
-    df.columns = df.columns.map(resp[0])
-
-    # Linhas de código para tratamento de variáveis removidas até que eu compreenda se tal tratamento seria possível.
-
-    #pd.options.display.max_columns = 14
-
-    ### Corrige o índice de entradas do DataFrame
-    df.to_dict('records')
-    df = df.reset_index(drop=True)
-    print(df.dtypes)
-    #print(df.head())
-    print(df.tail())
 
 #####
 
-treat()
+
+def core(unt,p='last',v='allxp',t=None,c=None,cc=None,f=None,d=None,h=None):
+
+    # unt -> nível territorial + unidade territorial. Pode assumir inúmeros valores, separados por barras.
+    # (Ex: 'n1/1/n2/1').
+
+    # p -> períodos desejados.
+    # (Ex: '2008,2010-2012' – especifica os anos de 2008, e 2010 a 2012).
+
+    # v -> especifica o código das variáveis desejadas, separadas por vírgulas.
+    # (Ex: '643,1127')
+
+    # t -> código do agregado de onde serão retirados os dados para as variáveis desejadas. Pode ser obtido pela API de agregados.
+    # (Ex: '1327')
+
+    # c -> classificação
+    # cc -> categoria
+
+    v_string = v
+    if type(v) == list:
+        v_string = str()
+        for i in range(len(v)):
+            if i != (len(v)-1):
+                v_string = v_string + str(v[i]) + ","
+            else:
+                v_string = v_string + str(v[i])
+        print(v_string)
+
+    url='http://api.sidra.ibge.gov.br/values' + '/p/' + str(p) + '/' + str(unt) + '/v/' + str(v_string)
+
+    if t != None:
+        if str(t).isdigit():
+            url = url + '/t/' + str(t)
+
+        else:
+            pesqid = None
+            agregados = requests.get('https://servicodados.ibge.gov.br/api/v3/agregados')
+            agregadosdata = json.loads(agregados.text)
+            for i in range(len(agregadosdata)):
+                for j in range(len(agregadosdata[i]["agregados"])):
+                    if agregadosdata[i]["agregados"][j]["nome"] == str(t):
+                        pesqid = (agregadosdata[i]["agregados"][j]["id"])
+                        url = url + '/t/' + str(pesqid)
+                        break
+                    elif agregadosdata[i]["agregados"][j]["nome"] != str(t) and j == (
+                            len(agregadosdata[i]["agregados"][j]) - 1):
+                        continue
+                    elif agregadosdata[i]["agregados"][j]["nome"] != str(t) \
+                            and i == (len(agregadosdata) - 1) \
+                            and j == (len(agregadosdata[i]["agregados"][j]) - 1):
+                        print("Não há esse agregado de dados na base disponível")
+
+    if c != None and cc != None:
+        url = url + '/' + str(c) + '/' + str(cc)
+
+    if f != None:
+        url = url + '/f/' + str(f)
+
+    if d != None:
+        url = url + '/d/' + str(d)
+
+    if h != None:
+        url = url + '/h/' + str(h)
+
+
+    print(url)
+
+    data = requests.get(url)
+    datares = json.loads(data.text)
+    treat(datares)
+    return datares
+
+
+core("N1103/1","last",[331,1000331],1327)
